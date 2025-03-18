94f9cf9f81de7635fb3572ad7ed239a071a3b928
Bhargav <vyasb1503@gmail.com>
2025-03-18 23:42:08 +0530

    implementhing the controller search entity

--- src/main/java/com/tka/Controler/FlightConroler.java
+++ src/main/java/com/tka/Controler/FlightConroler.java
@@ -1,15 +1,25 @@
 package com.tka.Controler;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tka.Model.Flight;
+import com.tka.Service.FlightService;
 @RestController
 @RequestMapping("/flights")
 
 public class FlightConroler {
 	@Autowired
 	private FlightService flightService;
+	
+	@GetMapping("/")
+	public List<Flight> searchFlights(@RequestParam String departureAirport,)
 	
 
 }
