ced51b99a73f137c39fc8f5792bde256790ba7bd
Bhargav <vyasb1503@gmail.com>
2025-03-18 23:26:34 +0530

    implementhing the  is the flight entity

--- src/main/java/com/tka/Model/Flight.java
+++ src/main/java/com/tka/Model/Flight.java
@@ -8,10 +8,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Table(name ="flights")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Flight {
 	@Id
 	@GeneratedValue (strategy =  GenerationType.IDENTITY)
